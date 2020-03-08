e7faaebbdbdbd80e691622056de5f9754cdd5e93
bwernick <bwernick@csumb.edu>
2020-03-07 20:07:43 -0800

    animation stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,14 +13,18 @@ public class Player : MonoBehaviour
     public float fireRate;
     private float nextFire;
 
+    Animator player_animator;
+
     void Start()
     {
         player = GetComponent<Transform>();
+        player_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         float h = Input.GetAxis("Horizontal");
 
         //Prevent player from leaving screen bounds
@@ -42,6 +46,9 @@ public class Player : MonoBehaviour
             nextFire = Time.time + fireRate;
             Instantiate(bullet, shottingOffset.position, shottingOffset.rotation);
             Debug.Log("Bang!");
+            player_animator.SetBool("player_shoot", true);
         }
+
+        player_animator.SetBool("player_shoot", false);
     }
 }
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Start Game");
+            SceneManager.LoadScene("Game");
+        }
+
+    }
+}
